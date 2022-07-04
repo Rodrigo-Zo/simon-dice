52fcbe8b985ac943d99ab32e1597e9b74ce80fc1
Rodrigo <zoccorodrigo@gmail.com>
2022-07-04 10:13:24 -0300

    Actualiza el proyecto
    completa la lógica del proyecto, falta coordinar los timeout y arreglar la lógica para pasar de turno.

--- index.js
+++ index.js
@@ -1,81 +1,99 @@
 function ocultarElemento(elemento){
     elemento.className = 'oculto';
 }
+
 function desocultarElemento(elemento){
     elemento.className = '';
 }
 
-function comenzarJuego(){
-    ocultarElemento($botonEmpezar);   
+function actualizarRondaDisplay(ronda){
+    let rondaDisplay = document.querySelector('#ronda');
+    rondaDisplay.textContent = ronda;
+}
+
+function actualizarTurnoDisplay(turno){
+    let turnoDisplay = document.querySelector('#turno');
+    turnoDisplay.textContent = turno;
+}
+
+function pintarColor($color){
+    $color.style.opacity = 1;
+    
+    setTimeout(function() {
+        $color.style.opacity = 0.3;
+    }, 1000);
 }
 
 function obtenerColorRandom(){
-    $colorRandom = $colores[Math.floor(Math.random() * $colores.length)];
+    numeroRandom = Math.floor(Math.random() * 4);
+    $colorRandom = $colores[numeroRandom];
     coloresMaquina.push($colorRandom);
 }
 
 function pintarColoresMaquina(){
-    coloresMaquina.forEach(function($color, indice){
-        $color.style.opacity = 1;
+    coloresMaquina.forEach(function($color, index){
+        let retraso = (index + 1) * 750;
         
         setTimeout(function(){
-            $color.style.opacity = 0.3;
-        },(indice + 1) * 1000);
+            pintarColor($color);
+        }, retraso);
 
     });
 }
 
 function hacerTurnoMaquina(){
-    ronda++;
-    actualizarRonda(ronda);
-    actualizarTurno('Es el turno de la maquina');
+    ronda ++;
+    actualizarRondaDisplay(ronda);
+    actualizarTurnoDisplay('Turno de la máquina');
     obtenerColorRandom();
-    
-    setTimeout(function(){
-        pintarColoresMaquina();
-    }, 1000);
-
-}
-
-function hacerTurnoJugador(){
-    setTimeout(function(){
-        actualizarTurno('Te toca');
-    },(coloresMaquina.length + 1) * 1250);
-
-    elegirColorUsuario();
-
+    pintarColoresMaquina();
 }
 
 function elegirColorUsuario(){
     $colores.forEach(function($color){
         $color.onclick = elegirColor;
-
     });
 }
 
+function hacerTurnoUsuario(){
+actualizarTurnoDisplay('Te Toca');
+elegirColorUsuario();
+}
+
 function elegirColor(e){
-    let $colorElegido = e.target;
-    coloresUsuario.push($colorElegido);
-    pintarColor($colorElegido)
+    let indice = 0;
+    let $colorUsuario = e.target;
+    pintarColor($colorUsuario);
+
+    if($colorUsuario.id !== coloresMaquina[indice].id){
+        console.log('perdiste');
+    }
+
+    if($colorUsuario.id === coloresMaquina[indice].id){
+        indice ++;
+        coloresUsuario.push($colorUsuario);
+    }
+
+    if(coloresUsuario.length === coloresMaquina.length){
+        coloresUsuario = [];
+        indice = 0;
+        hacerTurnoMaquina();
+    }
 }
 
-function pintarColor($color){
-    $color.style.opacity = 1;
-    setTimeout(function(){;
-        $color.style.opacity = 0.3;
-    }, 1000);
-}
 
-
-let ronda = 0;
-let coloresMaquina = [];
-let coloresUsuario = [];
 $colores = document.querySelectorAll('.color');
+let ronda = 0;
+let coloresUsuario = [];
+let coloresMaquina = [];
+
 $botonEmpezar = document.querySelector('#boton-empezar');
 
 $botonEmpezar.onclick = function(){
-    comenzarJuego();
+    ocultarElemento($botonEmpezar);
     hacerTurnoMaquina();
-    hacerTurnoJugador();
-}
 
+    setTimeout(function(){
+        hacerTurnoUsuario();
+       },(coloresMaquina.length + 1) * 1000);
+}
